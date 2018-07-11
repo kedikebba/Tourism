be6986a683a4cc62bbf8518436f901c1c475422b
emukungu <enmukungu@gmail.com>
2018-07-11 15:38:09 +0300

    style slideshow images.

--- js/scripts.js
+++ js/scripts.js
@@ -13,12 +13,8 @@ $(document).ready(function(){
       }
       slideIndex++;
       if (slideIndex > slides.length) {slideIndex = 1}
-      // for (i = 0; i < captions.length; i++) {
-      //     captions[i].className = captions[i].className.replace("");
-      // }
       slides[slideIndex-1].style.display = "block";
-      // captions[slideIndex-1].className += " active";
-      setTimeout(showSlides, 3000); // Change image every 2 seconds
+      setTimeout(showSlides, 3000); // Change image every 3 seconds
     }
 
 })
