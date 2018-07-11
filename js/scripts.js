d7d4ea8d9bad3ce75d9496fdda3b1a5befb4763a
emukungu <enmukungu@gmail.com>
2018-07-11 10:38:27 +0300

    add slides shows to the home page.

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+// //BUSINESS LOGIC
+// function showSlides() {
+//     var slideIndex = 0;
+//     var i;
+//     var slides = document.getElementsByClassName("slideShow");
+//     // var dots = document.getElementsByClassName("dot");
+//     for (i = 0; i < slides.length; i++) {
+//        slides[i].style.display = "none";
+//     }
+//     slideIndex++;
+//     if (slideIndex > slides.length) {slideIndex = 1}
+//     // for (i = 0; i < dots.length; i++) {
+//     //     dots[i].className = dots[i].className.replace(" active", "");
+//     // }
+//     slides[slideIndex-1].style.display = "block";
+//     // dots[slideIndex-1].className += " active";
+//     setTimeout(showSlides, 2000); // Change image every 2 seconds
+// }
+//
+//
+// //USER LOGIC
+// // $(document).ready(function(){
+// //   var slideIndex = 0
+// //   showSlides()
+// //
+// // })
+//   var slideIndex = 0;
+//   showSlides();
+
+$(document).ready(function(){
+  var slideIndex = 0;
+  showSlides();
+
+  function showSlides() {
+      var i;
+      var slides = document.getElementsByClassName("slideShow");
+      var captions = document.getElementsByClassName("caption");
+      for (i = 0; i < slides.length; i++) {
+         slides[i].style.display = "none";
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1}
+      // for (i = 0; i < captions.length; i++) {
+      //     captions[i].className = captions[i].className.replace("");
+      // }
+      slides[slideIndex-1].style.display = "block";
+      // captions[slideIndex-1].className += " active";
+      setTimeout(showSlides, 2000); // Change image every 2 seconds
+    }
+
+})
