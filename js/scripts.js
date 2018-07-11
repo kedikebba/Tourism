67581de4384452086b15cd976ba18030f1427215
emukungu <enmukungu@gmail.com>
2018-07-11 11:13:23 +0300

    make the image information div static.

--- js/scripts.js
+++ js/scripts.js
@@ -45,7 +45,7 @@ $(document).ready(function(){
       // }
       slides[slideIndex-1].style.display = "block";
       // captions[slideIndex-1].className += " active";
-      setTimeout(showSlides, 2000); // Change image every 2 seconds
+      setTimeout(showSlides, 3000); // Change image every 2 seconds
     }
 
 })
