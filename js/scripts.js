d00f8533f7c8ad090d1eb31628e6e62223f0f5a6
emukungu <enmukungu@gmail.com>
2018-07-11 13:18:31 +0300

    add a sticky navigation bar below the first page section.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,5 @@
 // //BUSINESS LOGIC
-// function showSlides() {
-//     var slideIndex = 0;
-//     var i;
-//     var slides = document.getElementsByClassName("slideShow");
-//     // var dots = document.getElementsByClassName("dot");
-//     for (i = 0; i < slides.length; i++) {
-//        slides[i].style.display = "none";
-//     }
-//     slideIndex++;
-//     if (slideIndex > slides.length) {slideIndex = 1}
-//     // for (i = 0; i < dots.length; i++) {
-//     //     dots[i].className = dots[i].className.replace(" active", "");
-//     // }
-//     slides[slideIndex-1].style.display = "block";
-//     // dots[slideIndex-1].className += " active";
-//     setTimeout(showSlides, 2000); // Change image every 2 seconds
-// }
-//
-//
-// //USER LOGIC
-// // $(document).ready(function(){
-// //   var slideIndex = 0
-// //   showSlides()
-// //
-// // })
-//   var slideIndex = 0;
-//   showSlides();
-
+//USER LOGIC
 $(document).ready(function(){
   var slideIndex = 0;
   showSlides();
