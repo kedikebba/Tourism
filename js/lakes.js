e0f580927141f2c13e69df17ee544cfddb125536
Kedi Kebba <kebbakedi@gmail.com>
2018-07-13 11:12:47 +0300

    Adds footer and header to pages

---
+++ js/lakes.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function(){
+$('#lakesGallery').addClass('animated rotateInDownLeft')
+
+
+  var $item = $('.item');
+  var $wHeight = $(window).height();
+
+  $item.height($wHeight);
+  $item.addClass('full-screen');
+
+  $('.carousel img').each(function() {
+    var $src = $(this).attr('src');
+    var $color = $(this).attr('data-color');
+    $(this).parent().css({
+      'background-image' : 'url(' + $src + ')',
+      'background-color' : $color
+      // 'linear-gradient( rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), '
+    });
+    $(this).remove();
+  });
+
+  $(window).on('resize', function (){
+    $wHeight = $(window).height();
+    $item.height($wHeight);
+  });
+  // $('video').playbackRate = 1.5;
+
+
+});
